3fa133e142dfa7796e6a040822df0042595c0c80
Yasnohorodskyi <62336521+Yasnohorodskyi@users.noreply.github.com>
2021-04-22 18:34:40 +0300

    Автоматизация сборки. Ускорение загрузки сайта

--- source/js/map.js
+++ source/js/map.js
@@ -1,29 +1,35 @@
-const center = [59.93881322008863, 30.322945474580138];
-const zoom = 17;
+const map = document.querySelector('.map');
 
-const myMap = L.map('mapid').setView(center, zoom);
+map.addEventListener('mouseover', function() {
 
-layer = googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
-  maxZoom: 20,
-  subdomains:['mt0','mt1','mt2','mt3']
-});
+  const center = [59.93881322008863, 30.322945474580138];
+  const zoom = 17;
 
-myMap.addLayer(layer);
+  const myMap = L.map('mapid').setView(center, zoom);
 
-const iconOptions = {
-  iconUrl: '../img/map-pin.svg',
-  iconSize: [67, 100],
-  iconAnchor:   [25, 85]
-}
+  layer = googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+  });
 
-const customIcon = L.icon(iconOptions);
+  myMap.addLayer(layer);
 
-const markerOptions = {
-  title: "MyLocation",
-  clickable: true,
-  draggable: true,
-  icon: customIcon
-}
+  const iconOptions = {
+    iconUrl: '../img/map-pin.svg',
+    iconSize: [67, 100],
+    iconAnchor:   [25, 85]
+  }
 
-const marker = new L.Marker(center, markerOptions);
-marker.addTo(myMap);
+  const customIcon = L.icon(iconOptions);
+
+  const markerOptions = {
+    title: "MyLocation",
+    clickable: true,
+    draggable: true,
+    icon: customIcon
+  }
+
+  const marker = new L.Marker(center, markerOptions);
+  marker.addTo(myMap);
+})
+
